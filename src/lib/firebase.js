304ce78f12ef4bc08b1c8ec081c225eb9eaa8e77
Pekorea <okatahi1018@gmail.com>
2023-08-24 16:50:51 +0100

    update

---
+++ src/lib/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAGpcMCqamyYhJKa9QCVyEaJVCC4Yr_cIg",
+  authDomain: "notebook-b367a.firebaseapp.com",
+  projectId: "notebook-b367a",  
+  storageBucket: "notebook-b367a.appspot.com",
+  messagingSenderId: "398657830681",
+  appId: "1:398657830681:web:75c80a389bcfcac4f5cf88",
+  measurementId: "G-5EV0F0S62F"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
---
+++ src/pages/loginsignup.js
@@ -0,0 +1,90 @@
+import {useState} from 'react'
+import {Toaster, toast} from 'react-hot-toast'
+
+export default function Validation(){
+    const [name, setName] = useState([])
+    const [pass, setPassword] = useState([])
+    const [npass, setNpass] = useState([])
+    const [email, setEmail] = useState([])
+    const [view, setView] = useState([])
+
+    const success = ()=>{toast('Successfully logged in',{duration:3000, icon:'✔👍'})}
+    
+    const handleEmail =(e)=>{
+        setEmail(e.target.value)
+    }
+    const handlePass =(e)=>{
+        setPassword(e.target.value)
+    }
+
+    const handleChecked =()=>{
+        setView(!view)
+    }
+    const handlesub=(e)=>{
+        e.preventDefault()
+        success()
+
+    }
+    
+    return(
+        <div>
+            <form onSubmit={handlesub}>
+        <div className='mainContainer'>
+            <Toaster/>
+            
+            <div className='valds'>
+
+            <div className='maincontvalds'>
+            <div className='form'>
+            <div className='headercont'>
+                <h1 className='login'>LOGIN</h1>
+            </div>
+            <hr></hr>
+
+            <div className='inputs'>
+
+            <div className='emailInputdiv'>
+
+            <input
+            type='email'
+            placeholder=''
+            className='emailInput'
+            onChange={handleEmail}
+            value={email}
+            required
+            ></input>
+            <label>Email</label>
+            </div>
+
+            <div className='passwordInputdiv'>
+            <input
+            value={pass}
+            onChange={handlePass}
+            type={view?'password':'text'}
+            className='passwordInput'
+            required
+            ></input>
+            <label>Password</label>
+            </div>
+            <div className='checkbos'>
+            <input className='viewpass' type='checkbox' onChange={handleChecked}></input><label className='view' htmlFor='viewpass'>{view?`Show`:`Hide`} password</label>
+            </div>
+            <div className='logbtndiv'>
+            <button className='logbtn' type='submit'> PROCEED</button>
+            </div>
+
+            </div>
+            </div>
+
+            </div>
+            </div>
+           
+            <div className='imgcont'>
+                <h2>THE NOTEBOOK</h2>
+            </div>
+
+
+        </div> </form>
+        </div>
+    )
+}
