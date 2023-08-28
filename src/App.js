7c8522c65492eeb08a8c0ba7f3c0bd58b021f664
Pekorea <okatahi1018@gmail.com>
2023-08-28 12:24:37 +0100

    sigh

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+
 import Signup from "./pages/Signup";
 import Forgotten_password from "./pages/forgottenp";
 import Validation from "./pages/loginsignup";
@@ -6,7 +7,7 @@ import Validation from "./pages/loginsignup";
 function App() {
   return (
     <div>
-      <Forgotten_password />
+      <Signup />
     </div>
   );
 }
