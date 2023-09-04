d472dd7f51d6a90403acfda76edf7b26918714d8
Pekorea <okatahi1018@gmail.com>
2023-09-04 22:48:14 +0100

    update

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,10 +1,17 @@
 import '../designs/homemindev.css'
 import '../designs/home.css'
+import Navbar from '../components/navbar'
 
 function Home() {
   return (
     <div className="cont">
-        
+        <Navbar/>
+
+        <div className='mainNc'>
+        <div className='notescont'>
+            <p>sighhh</p>
+        </div>
+        </div>
         
     </div>
   )
