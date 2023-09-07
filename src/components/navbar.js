b791321ea7960f9b374ddc06db613b92ded22404
Pekorea <okatahi1018@gmail.com>
2023-09-07 09:10:34 +0100

    searchbar done

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,11 +1,24 @@
 import {TiThMenu} from 'react-icons/ti'
 import {BiSearchAlt} from 'react-icons/bi'
+import { useState } from 'react'
 
 const Navbar = () => {
+  const [searchT,setSearchT]=useState(false);
+  const [onSearch,setOnSearch]=useState("");
+  //const [search,setSearch]=useState("");
 
+  const searching =(event)=>{
+    setOnSearch(event.target.value)
+  }
   
+  const isOpen =()=>{
+    setSearchT(!searchT)
+  }
+
   return (
+    <div>
     <div className='navbar'>
+      <div className='navcont'>
         <header className='navbar-head'>
           <TiThMenu className='menubar'/>
           <h1 style={{
@@ -16,13 +29,25 @@ const Navbar = () => {
             
             >THE NOTEBOOK</h1>
         </header>
-        <BiSearchAlt className='searchIcon'/>
+        <button onClick={isOpen} className='searchIcondiv'><BiSearchAlt className='searchIcon'/></button>
         <ul className='list-items'>
           <li>About</li>
          
 
         </ul>
+      </div>
         
+      <div className={searchT ? 'searchBar' : 'searchbar'}>
+          <input
+          type='search'
+          value={onSearch}
+          onChange={searching}
+
+          ></input>
+          
+        </div>
+        
+    </div>
     </div>
   )
 }
