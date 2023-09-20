12be96e7e6daeceb207ec9ec412ec02ff717fa00
Pekorea <okatahi1018@gmail.com>
2023-09-20 16:29:07 +0100

    apt

--- src/components/header.js
+++ src/components/header.js
@@ -1,27 +1,14 @@
 import "../designs/home.css";
 import "../designs/homemindev.css";
+import { BsBook } from "react-icons/bs";
 const Header = () => {
   return (
     <div className="nothead_div">
       <header className="nothead">
         THE NOTEBOOK
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="svgnotebook"
-          style={{ position: "absolute", top: "0.4rem", left: "31.2rem" }}
-          width="28"
-          height="29"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          class="lucide lucide-book-open"
-        >
-          <path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z" />
-          <path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z" />
-        </svg>
+        <BsBook
+          style={{ width: "2rem", height: "2.4rem", paddingLeft: "6px" }}
+        />
       </header>
     </div>
   );
